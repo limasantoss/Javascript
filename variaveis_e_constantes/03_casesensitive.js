38a35c2d5bf63ee2fe75cfe20a02674fd79ce479
Igor Santos <igorlimasantos667@gmail.com>
2025-06-11 17:01:31 -0300

    casesensiteve.js

---
+++ variaveis_e_constantes/03_casesensitive.js
@@ -0,0 +1,11 @@
+var product = "teclado mecanico";
+var Product = "Mouse Gamer"
+
+var product = "Fone sem fio" /*VAI SOBRESCREVER O TECLADO GAMER */
+
+console.log(product)
+console.log(Product)
+
+/* Quando uma linguagem de programação é 
+case -sensiteve significa que ela é sensivel a letras maiúsculas
+e minúsculas */

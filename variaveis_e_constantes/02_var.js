623149311b2a1704937ea4eadba634540b2edddf
Igor Santos <igorlimasantos667@gmail.com>
2025-06-11 16:54:22 -0300

    variavieseconstantes.js

---
+++ variaveis_e_constantes/02_var.js
@@ -0,0 +1,13 @@
+// Declara uma variável sem valor.
+var user 
+
+console.log(user)
+
+
+//Delarar uma variável com valor.
+var email = "igorlima@gmail.com"
+console.log(email)
+
+//Substitui o valor da variável.
+email = "joao@email.com"
+console.log(email)

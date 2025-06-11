3ddc4c4961f871b5445e5b7581ae1c3a1a09edad
Igor Santos <igorlimasantos667@gmail.com>
2025-06-11 17:25:21 -0300

    let.js

---
+++ variaveis_e_constantes/04_let.js
@@ -0,0 +1,12 @@
+//Declarar uma variável sem valor
+
+let user 
+console.log("Igor Lima ")
+
+
+//Declara uma variável com valor.
+let email = "Igorlima@email.com"
+console.log(email)
+
+email = "igor@gmail.com"
+console.log(email)

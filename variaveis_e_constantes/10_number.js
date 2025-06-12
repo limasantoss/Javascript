de2f948a690cfa27801b787d53543c68013b1189
Igor Santos <igorlimasantos667@gmail.com>
2025-06-12 00:59:45 -0300

    number.js

---
+++ variaveis_e_constantes/10_number.js
@@ -0,0 +1,15 @@
+
+/// number 
+console.log(typeof 5)
+
+//inteiro positivo
+console.log(5)
+
+//inteiro negativo
+console.log(-5)
+
+//numeros reais ou float
+console.log(125.70)
+
+//NAM-NOT a number
+console.log(12.5/'igor')

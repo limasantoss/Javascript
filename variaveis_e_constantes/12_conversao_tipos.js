0662695e82222b5a3afbc5e3ef515d68c4a9ccdd
Igor Santos <igorlimasantos667@gmail.com>
2025-06-12 02:35:15 -0300

    conversaodetipos.js

---
+++ variaveis_e_constantes/12_conversao_tipos.js
@@ -0,0 +1,13 @@
+///type casting ou type conversion . 
+
+
+let value ="9"
+console.log(typeof value)
+console.log(typeof Number(value))
+
+
+let age = 18;
+console.log(typeof age.toString())
+
+let option = -1
+console.log(Boolean(option))

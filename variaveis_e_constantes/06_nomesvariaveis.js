34b9d583177cc1e1907557e347f5d662d4670b89
Igor Santos <igorlimasantos667@gmail.com>
2025-06-12 00:03:25 -0300

    nomesvariaveis.js

---
+++ variaveis_e_constantes/06_nomesvariaveis.js
@@ -0,0 +1,26 @@
+let username = "Igor"
+let userName = "João"
+
+
+console.log(username)
+console.log(userName)
+
+
+//Podemos
+
+let $email = "igorlima@gmail.com"
+let _email = "igorlima@gmail.com"
+let user_email ="igorlima@gmail.com"
+
+console.log($email)
+console.log(_email)
+console.log(userName)
+
+//não podemos
+//let luser = "Ana" boas praticas , dar nome que dê sentido !
+
+//RECOMENDAÇÕES camelCase(corcunda do Camelo)
+let productName  = "Teclado"
+let firstName = "Igor"
+let lastName = "Lima"   //dar nome que dê sentido !
+

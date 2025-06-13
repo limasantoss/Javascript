ad170eac20e26ca265b65aba28732cfc15d95b01
Igor Santos <igorlimasantos667@gmail.com>
2025-06-13 03:05:11 -0300

    maiormenoreigual.js

---
+++ expressoes_e_operadores/08_maior_menor_igual.js
@@ -0,0 +1,15 @@
+ let balance = 500
+ let payment = 120
+
+ // >maior que
+ console.log(balance > payment)
+ 
+ //< menor que
+ console.log(balance < payment)
+
+//>=Maior ou igual a
+ console.log(balance >= payment)
+
+ // <= Menor ou igual a 
+ balance <= 500
+ console.log(balance <= payment)

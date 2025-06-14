d12cd8915952f8dbecd028f3fcb0ac832bb92c13
Igor Santos <igorlimasantos667@gmail.com>
2025-06-14 16:33:23 -0300

    operadorlogicos.js

---
+++ expressoes_e_operadores/10_operador_logicos.js
@@ -0,0 +1,14 @@
+let email = true
+let passoword = false
+
+
+//and (E) &&
+console.log(email && passoword )
+
+//or (ou) ||
+
+console.log(email || passoword)
+
+//NOT (NEGACAO) !
+
+console.log(!passoword)

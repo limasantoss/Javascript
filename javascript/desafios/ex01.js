e62316d4f5ab7705fff89154701779397ebc632e
Igor Santos <igorlimasantos667@gmail.com>
2025-06-14 23:04:02 -0300

    ex01.js

---
+++ javascript/desafios/ex01.js
@@ -0,0 +1,7 @@
+//Elabore um algoritmo e some dois números e mostre a soma deles
+
+const numberOne = 10;
+const numberTwo = 20;
+const result = numberOne + numberTwo
+
+console.log(`Resultado de ${numberOne} + ${numberTwo} é = ${result}`)

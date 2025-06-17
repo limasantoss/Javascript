64462abf644a3bd8118411c1327a23e5e3ca19da
Igor Santos <igorlimasantos667@gmail.com>
2025-06-17 09:14:14 -0300

    t

---
+++ javascript/desafios/ex07.js
@@ -0,0 +1,7 @@
+//6. Controle de Escopo em Funções
+function x (){
+    let Mensagem ="oi"
+    console.log(Mensagem)//Aqui funciona
+}
+x()
+console.log(Mensagem) // só vai funcionar dentro da função  , pois foi criada e amarzenada dentro da função

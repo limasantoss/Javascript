a2e72c671bb8b6b815eb628da2bdaed20f6088ca
Igor Santos <igorlimasantos667@gmail.com>
2025-06-21 22:38:23 -0300

    Update 08tratamento_de_exceçoes.js

---
+++ javascript/condicionais_e_controle_de_fluxo/08tratamento_de_exceçoes.js
@@ -0,0 +1,18 @@
+console.log(result)
+
+
+try{
+//tenta executar algo
+console.log(result)
+}catch(error){
+
+///captura o erro para tratar
+
+console.log("erro")
+
+}
+finally {
+    console.log("fim")
+}
+
+

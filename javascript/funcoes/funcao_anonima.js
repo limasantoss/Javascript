4953af516aa179224873a2da9dbcd1017339c02c
Igor Santos <igorlimasantos667@gmail.com>
2025-06-22 10:55:28 -0300

    .

---
+++ javascript/funcoes/funcao_anonima.js
@@ -0,0 +1,11 @@
+const showMessage1 = function(){
+    return "Olá , Igor"
+}
+
+console.log(showMessage1())
+
+
+const showMessage2 = function(message, name){
+    return message + name 
+}
+console.log(showMessage2("Olá " , " joao"))
